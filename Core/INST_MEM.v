03b7b9a9330ac118ed2479779c4649941a892300
ike <3214140360@qq.com>
2023-06-07 00:21:02 +0800

    回退一下啊

--- Core/INST_MEM.v
+++ Core/INST_MEM.v
@@ -1,21 +1,12 @@
 `include "./Core/DEFINES.v"
 module inst_mem (
-    input mem_wen, clk,
-    input [`REG_WIDTH -1:0] reg_w_data,
     input [`REG_WIDTH -1:0] pc,
-    input [`REG_WIDTH -1:0] mem_r_addr,
-    input [`REG_WIDTH -1:0] mem_w_addr,
-    output reg [`REG_WIDTH -1:0] ir,
-    output reg [`REG_WIDTH -1:0] mdr
+    output reg [`REG_WIDTH -1:0] ir
 );
 
   reg [`REG_WIDTH -1:0] all_inst[`INST_MEM_ADDR_DEPTH - 1:0];
-  always @(posedge clk) begin
-    ir <= all_inst[pc[`INST_MEM_ADDR_WIDTH+2-1:2]];
-    if (mem_wen) begin
-      all_inst[mem_w_addr[`REG_WIDTH -1:0]] <= reg_w_data;
-    end
-    mdr <= all_inst[mem_r_addr[`REG_WIDTH -1:0]]; 
+  always @(*) begin
+    ir = all_inst[pc[`INST_MEM_ADDR_WIDTH+2-1:2]];
   end
 
 endmodule
